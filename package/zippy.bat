815a25d851854428d2561538c39d172fc7171d17
Nahum Timerman <ntimerman@gmail.com>
2016-09-13 13:23:59 +0300

    changes to datamodel

--- package/zippy.bat
+++ package/zippy.bat
@@ -1,2 +1,11 @@
-del C:\work\git\cloudshell-traffic-teravm\drivers\controller\src\src.zip /F
-"C:\Program Files\7-Zip\7z.exe" a -r C:\work\git\cloudshell-traffic-teravm\drivers\controller\src\src.zip -w C:\work\git\cloudshell-traffic-teravm\drivers\controller\src\*.* -mem=AES256
+
+
+del "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\TeraVM Controller.zip" /F
+del "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\TeraVM Management Assistant.zip" /F
+del "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\TeraVM Test Module.zip" /F
+del "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\Deploy TeraVM Instance.zip" /F
+
+"C:\Program Files\7-Zip\7z.exe" a -r "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\TeraVM Controller.zip" -w C:\work\git\cloudshell-traffic-teravm\drivers\controller\src\*.* -mem=AES256
+"C:\Program Files\7-Zip\7z.exe" a -r "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\Deploy TeraVM Instance.zip" -w C:\work\git\cloudshell-traffic-teravm\drivers\deployment_drivers\teravm_instance\*.* -mem=AES256
+"C:\Program Files\7-Zip\7z.exe" a -r "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\TeraVM Management Assistant.zip" -w C:\work\git\cloudshell-traffic-teravm\drivers\teravm_management_assistant\src\*.* -mem=AES256
+"C:\Program Files\7-Zip\7z.exe" a -r "C:\work\git\cloudshell-traffic-teravm\cloudshell_traffic_teravm_Package\Resource Drivers - Python\TeraVM Test Module.zip" -w C:\work\git\cloudshell-traffic-teravm\drivers\test_module\src\*.* -mem=AES256
